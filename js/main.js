58671cbeb30f6790a6f4082a9946ca379157f9b0
Skk-martinGarcia <martinezequielgarcia495@gmail.com>
2023-08-21 20:42:54 -0300

    proyecto finalizado

--- js/main.js
+++ js/main.js
@@ -38,8 +38,24 @@ document.addEventListener("keydown", function (event) {
           }         
 });
 
-const dollM4 = document.querySelector("#dollM4");                 // Lo siento mucho por el programador de JS que vea esto                    
-const dollM16 = document.querySelector("#dollM16A1");             // Juro que voy a aprender funciones 
+const ARfaction = document.querySelector("#ARteam");
+const Four04faction = document.querySelector("#Team404");
+const ARgrid = document.querySelector("#gridA");
+const Four04grid = document.querySelector("#gridB");
+
+ARfaction.addEventListener("click", () => { 
+    ARgrid.classList.remove("woosh");
+    Four04grid.classList.remove("visible");
+})
+
+Four04faction.addEventListener("click", () => { 
+    Four04grid.classList.add("visible");
+    ARgrid.classList.remove("visible");
+    ARgrid.classList.add("woosh");
+})
+
+const dollM4 = document.querySelector("#dollM4");                 // Lo siento mucho por el programador que sabe JS que vea esto                    
+const dollM16 = document.querySelector("#dollM16A1");             // Juro que voy a aprender funciones y lo necesario para poder reducir el tamaño de este desastre
 const doll15 = document.querySelector("#dollAR15");
 const dollSOP = document.querySelector("#dollSOP2");
 const dollRO = document.querySelector("#dollRO635");
@@ -89,5 +105,58 @@ dollRO.addEventListener("click", () => {
     AR15desc.classList.remove("visible");
     SOPdesc.classList.remove("visible");
     ROdesc.classList.add("visible");
+})
+
+const dollUMP45 = document.querySelector("#dollUMP45");                               
+const dollUMP9 = document.querySelector("#dollUMP9");            
+const doll416 = document.querySelector("#doll416");
+const dollG11 = document.querySelector("#dollG11");
+const doll40 = document.querySelector("#doll40");
+
+const UMP45desc = document.querySelector("#desc1_45");
+const UMP9desc = document.querySelector("#desc2_9");
+const HK416desc = document.querySelector("#desc3_416");
+const G11desc = document.querySelector("#desc4_11");
+const unkwndesc = document.querySelector("#desc5_40");
+
+dollUMP45.addEventListener("click", () => { 
+    UMP45desc.classList.add("visible");
+    UMP45desc.classList.remove("woosh");
+    UMP9desc.classList.remove("visible");
+    HK416desc.classList.remove("visible");
+    G11desc.classList.remove("visible");
+    unkwndesc.classList.remove("visible");
+})
+
+dollUMP9.addEventListener("click", () => {
+    UMP45desc.classList.add("woosh");
+    UMP9desc.classList.add("visible");
+    HK416desc.classList.remove("visible");
+    G11desc.classList.remove("visible");
+    unkwndesc.classList.remove("visible");
+})
+
+doll416.addEventListener("click", () => {
+    UMP45desc.classList.add("woosh");
+    UMP9desc.classList.remove("visible");
+    HK416desc.classList.add("visible");
+    G11desc.classList.remove("visible");
+    unkwndesc.classList.remove("visible");
+})
+
+dollG11.addEventListener("click", () => {
+    UMP45desc.classList.add("woosh");
+    UMP9desc.classList.remove("visible");
+    HK416desc.classList.remove("visible");
+    G11desc.classList.add("visible");
+    unkwndesc.classList.remove("visible");
+})
+
+doll40.addEventListener("click", () => {
+    UMP45desc.classList.add("woosh");
+    UMP9desc.classList.remove("visible");
+    HK416desc.classList.remove("visible");
+    G11desc.classList.remove("visible");
+    unkwndesc.classList.add("visible");
 })
 
